d510435fd5dbc0e4b0adbe6f0cb88f7915ab9deb
Anthony Castrio <anthonycastrio@gmail.com>
2023-04-01 02:21:10 -0400

    integrate image gen module

--- gen_images.py
+++ gen_images.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 import openai
 
-def image_gen():
+def gen(prompt_list):
     load_dotenv()
 
     # Set up the OpenAI API client
@@ -13,15 +13,7 @@ def image_gen():
     prompt = 'Retelling of Rasputins death attempt - poison, bullets, and drowning.'
     prompt_2 = 'The rapper Ice Spice giving a presentation to a high school class'
 
-    prompt_list = [
-        'A hand-drawn animated portrait of Grigori Rasputin with his iconic beard and intense eyes.',
-        'A scene depicting Rasputin healing young Alexei while Tsar Nicholas II and Alexandra watch anxiously.',
-        'A montage of Rasputins controversial behavior, including him flirting with women and drinking heavily in a dimly lit bar.',
-        'A dramatic animation of the assassination attempt, showing Rasputin being offered poisoned wine and pastries, being shot multiple times, and being thrown into the icy Neva River.',
-        'A somber image of the frozen Neva River with the text "The End of Rasputin" appearing on the screen.'
-    ]
-
-    num = 3
+    num = 1
     for prompt_element in prompt_list:
         # Generate the image using the prompt
         response = openai.Image.create(prompt=prompt_element)
@@ -36,5 +28,5 @@ def image_gen():
         with open('generated_image_{}.jpg'.format(num), 'wb') as f:
             f.write(response.content)
 
-        num += 1
+        num+=1
 
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import gen_script
 import gen_audio
+import gen_images
+
+prompt_list = [
+    'A hand-drawn animated portrait of Grigori Rasputin with his iconic beard and intense eyes.',
+    'A scene depicting Rasputin healing young Alexei while Tsar Nicholas II and Alexandra watch anxiously.',
+    'A montage of Rasputins controversial behavior, including him flirting with women and drinking heavily in a dimly lit bar.',
+    'A dramatic animation of the assassination attempt, showing Rasputin being offered poisoned wine and pastries, being shot multiple times, and being thrown into the icy Neva River.',
+    'A somber image of the frozen Neva River with the text "The End of Rasputin" appearing on the screen.'
+]
+
 
 # [transcript, timestamps] = gen_script.gen()
 # print ("noice", transcript, timestamps)
 
-gen_audio.gen('output.mp3')
+# gen_audio.gen('output.mp3')
 
-
+gen_images.gen(prompt_list)
 
