a459a9ea8042f5ec2642fd960451b1c14a439463
Anthony Castrio <anthonycastrio@gmail.com>
2023-04-01 02:30:05 -0400

    update character prompts

--- gen_script.py
+++ gen_script.py
@@ -10,10 +10,9 @@ def gen():
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     character = {
-            "base": "Your job is to act like a college professor, accurately and concisely summarizing source information. You will be given one minute and 30 seconds to make a youtube short type of content. Mention any shocking or usual content. Avoid lengthy exposition and give more stories. Please provide an uninterrupted transcript without any cues like (enthusiastically). Anything you say should only be found in the transcript. After the transcript provide a description of the images you want playing in the background and their time stamps. Label the list of timestamped image descriptions like this: \n\n ::timestamps:: \n [00:00-00:20] description \n [00:21-00:52] description",
-        "icespice": "You are to act as Ice Spice. Ice Spice calls people munches."
+            "base": "Your job is to act like a college professor, accurately and concisely summarizing source information.\n You will be given 1000 characters towards generating a transcript for the information. The transcript should no include anything besides the exact words she would want spoken. This includes any expressions, exposition, or images.\n Mention any shocking or usual content. Avoid lengthy exposition and provide interesting stories.\n After the transcript, you will provide a transcript of specific and high quality image prompts for Dalle 2. All images should have a similar style and should include any text.",
+        "icespice": "You will be presenting the information like the rapper Ice Spice. She likes to call people munches."
     }
-
     user_input = "Your topic is: Rasputin. Your source material is: (https://en.wikipedia.org/wiki/Grigori_Rasputin)"
 
     # gpt-3.5-turbo	gpt-4
