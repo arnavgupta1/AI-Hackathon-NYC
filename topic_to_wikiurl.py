db9add295638991500afa27d6879757b8a3924ee
AayushGupta16 <123517745+AayushGupta16@users.noreply.github.com>
2023-04-01 09:47:42 -0400

    returns wikipedia url from input of topic

---
+++ topic_to_wikiurl.py
@@ -0,0 +1,20 @@
+import wikipediaapi
+
+def get_wikipedia_page(topic):
+    wiki = wikipediaapi.Wikipedia('en')
+    page = wiki.page(topic)
+    if page.exists():
+        return page.url
+    else:
+        return None
+
+'''
+if __name__ == "__main__":
+    topic = input("Enter a topic: ")
+    page = get_wikipedia_page(topic)
+    if page:
+       #print(f"Title: {page.title}")
+        print(f"URL: {page.fullurl}")
+    else:
+        print("No matching Wikipedia page found.")
+'''
