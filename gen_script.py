c04400ab67bc1d3b1904a77d464af1ed03546e34
Anthony Castrio <anthonycastrio@gmail.com>
2023-04-01 02:11:58 -0400

    restructure project

--- gen_script.py
+++ gen_script.py
@@ -3,41 +3,44 @@ from dotenv import load_dotenv
 import json
 import openai
 
-load_dotenv()
+def gen():
+    load_dotenv()
 
-openai.organization = "org-9gFweLPBiQroCILySsu6cZBQ"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.organization = "org-9gFweLPBiQroCILySsu6cZBQ"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-character = {
-        "base": "Your job is to act like a college professor, accurately and concisely summarizing source information. You will be given one minute and 30 seconds to make a youtube short type of content. Mention any shocking or usual content. Avoid lengthy exposition and give more stories. Please provide an uninterrupted transcript without any cues like (enthusiastically). Anything you say should only be found in the transcript. After the transcript provide a description of the images you want playing in the background and their time stamps. Label the list of timestamped image descriptions like this: \n\n ::timestamps:: \n [00:00-00:20] description \n [00:21-00:52] description",
-    "icespice": "You are to act as Ice Spice. Ice Spice calls people munches."
-}
+    character = {
+            "base": "Your job is to act like a college professor, accurately and concisely summarizing source information. You will be given one minute and 30 seconds to make a youtube short type of content. Mention any shocking or usual content. Avoid lengthy exposition and give more stories. Please provide an uninterrupted transcript without any cues like (enthusiastically). Anything you say should only be found in the transcript. After the transcript provide a description of the images you want playing in the background and their time stamps. Label the list of timestamped image descriptions like this: \n\n ::timestamps:: \n [00:00-00:20] description \n [00:21-00:52] description",
+        "icespice": "You are to act as Ice Spice. Ice Spice calls people munches."
+    }
 
-user_input = "Your topic is: Rasputin. Your source material is: (https://en.wikipedia.org/wiki/Grigori_Rasputin)"
+    user_input = "Your topic is: Rasputin. Your source material is: (https://en.wikipedia.org/wiki/Grigori_Rasputin)"
 
-# gpt-3.5-turbo	gpt-4
+    # gpt-3.5-turbo	gpt-4
 
-MODEL='gpt-3.5-turbo'
+    MODEL='gpt-4'
 
-completion = openai.ChatCompletion.create(
-  model=MODEL,
-  max_tokens=1200,
-  messages=[
-    {"role": "system", "content": character["base"]},
-    {"role": "system", "content": character["icespice"]},
-    {"role": "user", "content": user_input}
-  ]
-)
+    completion = openai.ChatCompletion.create(
+      model=MODEL,
+      max_tokens=1200,
+      messages=[
+        {"role": "system", "content": character["base"]},
+        {"role": "system", "content": character["icespice"]},
+        {"role": "user", "content": user_input}
+      ]
+    )
 
 
-content = completion.choices[0].message.content
+    content = completion.choices[0].message.content
 
-print("O", content)
-print("P", content.split('::timestamps::'))
+    # print("O", content)
+    # print("P", content.split('::timestamps::'))
 
-[transcript, timestamps] = content.split('::timestamps::')
+    [transcript, timestamps] = content.split('::timestamps::')
 
-print("A", transcript)
-print("B", timestamps)
+    # print("A", transcript)
+    # print("B", timestamps)
+
+    return [transcript, timestamps]
 
 
---
+++ main.py
@@ -0,0 +1,10 @@
+import gen_script
+import gen_audio
+
+# [transcript, timestamps] = gen_script.gen()
+# print ("noice", transcript, timestamps)
+
+gen_audio.gen('output.mp3')
+
+
+
