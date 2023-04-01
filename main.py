3443f088d56220e603746bea6cdcaef5cbd8d739
Anthony Castrio <anthonycastrio@gmail.com>
2023-04-01 11:40:58 -0400

    user interface

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 import gen_script
 import gen_audio
 import gen_images
+import topic_to_wikiurl
 
+CHARACTERS = {
+        "icespice": "You will be presenting the information like the rapper Ice Spice. She likes to call people munches."
+}
 
-# get user input for topic and character
+DEFAULT_CHARACTER = "icespice"
+DEFAULT_TOPIC = "Rasputin"
+DEFAULT_SOURCE = "https://en.wikipedia.org/wiki/Grigori_Rasputin"
 
-# generate the transcript and timestamped image descriptions
-print("generate script...")
-[transcript, image_prompts] = gen_script.gen() #outputted as lists
-# print (transcript, timestamps)
+if __name__ == "__main__":
+    # get user input for topic
+    user_topic = input("Enter a topic: ")
 
-# generate audio for the transcript
-print("generate audio...")
-audio_list = []
-for transcript_output in transcript:
-    #Stores all audio files in a list
-    audio_list.append(gen_audio.gen(transcript_output, 'output.mp3'))  
+    # get source for the user-provided topic
+    wiki_page = topic_to_wikiurl.get_wikipedia_page(user_topic)
+    if wiki_page:
+        topic = user_topic
+        source = wiki_page
+    else:
+        topic = DEFAULT_TOPIC
+        source = DEFAULT_SOURCE
+        print("No matching Wikipedia page found, defaulting to " + topic)
 
+    # get user input for character
+    print("These are the available voices: " + ', '.join(list(CHARACTERS.keys())))
+    character = input("Enter a character: ")
+    if character not in list(CHARACTERS.keys()):
+        print(character + " is not available, defaulting to " + DEFAULT_CHARACTER)
+        character = DEFAULT_CHARACTER
 
-topic="rasputin"
-print("generate images...")
-"""
-prompt_list = [
-    'A hand-drawn animated portrait of Grigori Rasputin with his iconic beard and intense eyes.',
-    'A scene depicting Rasputin healing young Alexei while Tsar Nicholas II and Alexandra watch anxiously.',
-    'A montage of Rasputins controversial behavior, including him flirting with women and drinking heavily in a dimly lit bar.',
-    'A dramatic animation of the assassination attempt, showing Rasputin being offered poisoned wine and pastries, being shot multiple times, and being thrown into the icy Neva River.',
-    'A somber image of the frozen Neva River with the text "The End of Rasputin" appearing on the screen.'
-]
-"""
+    # generate the transcript and timestamped image descriptions
+    print("generating script...")
+    [transcript, image_prompts] = gen_script.gen(character, topic, source) #outputted as lists
 
-gen_images.gen(topic, image_prompts)
+    # generate audio for the transcript
+    print("generating audio...")
+    audio_list = []
+    for transcript_output in transcript:
+        #Stores all audio files in a list
+        audio_list.append(gen_audio.gen(transcript_output, 'output.mp3'))  
+
+    # generate images for the video
+    print("generating images...")
+    gen_images.gen(topic, image_prompts)
+
+    # generate video by combining images, video, and audio
+    # TODO
 
